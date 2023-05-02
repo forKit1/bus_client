7bdd920b587461e4b2dc17153a8afb0b91f69ea2
forKit1 <79663419+forKit1@users.noreply.github.com>
2023-05-02 20:11:58 +0300

    carries add + rout

--- src/components/tableComponents/carries-data-table.vue
+++ src/components/tableComponents/carries-data-table.vue
@@ -1,19 +1,35 @@
 <template>
+  <button @click="showAddForm" class="add-button">Додати</button>
   <data-table :endpoint="endpointValue" :columns="columns"></data-table>
+    <div class="popup-container" v-if="addingNewData">
+    <carriers-data-table-add-form @cancel='cancel'></carriers-data-table-add-form>
+    </div>
 </template>
 
 <script>
+import carriersDataTableAddForm from './addForms/carriers-data-table-addForm.vue';
 import DataTable from './DataTable.vue';
+
 
 export default {
   name: 'App',
   components: {
       DataTable,
+      carriersDataTableAddForm,
   },
   data(){
     return{
+      addingNewData: false,
       endpointValue: "api/companies",
       columns : ['id', 'ownerId', 'name', 'vehicles']
+    }
+  },
+  methods:{
+    cancel(){
+      this.addingNewData = false;
+    },
+    showAddForm(){
+      this.addingNewData = true;
     }
   }
 
@@ -22,5 +38,16 @@ export default {
 </script>
 
 <style>
-
+.popup-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
 </style>
